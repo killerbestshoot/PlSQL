b15ac1ec2945f3c51fea5c7eccc51bf82b587d1f
killerbestshoot <101983731+killerbestshoot@users.noreply.github.com>
2022-06-30 13:25:00 -0400

    premier commit sql

---
+++ operation.sql
@@ -0,0 +1,7 @@
+Accept number1 prompt' Entrer le premier nombre ;'
+Accept number2 prompt' Entrer le second nombre ;'
+Declare
+Nb1 number(6) :=&nombre1;
+Nb2 number(6) :=&nombre2;
+Som number(7);
+
